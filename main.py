72269ca164068fc0db967ba66b847f7f2237d85d
RhysMarch <c1012902@newcastle.ac.uk>
2023-01-22 01:46:13 +0000

    Code gets up to the day that will have new slots available, but cannot choose an available slot yet.

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ def automated_booking():
     driver.get(
         'https://sportsbookings.ncl.ac.uk/Connect/mrmLogin.aspx')
     driver.maximize_window()
+
     # Login
     login_button = driver.find_element(By.XPATH, '//*[@id="form_1"]/div[1]/div/a')
     login_button.click()
     second_login_button = driver.find_element(By.XPATH, '//*[@id="Authenticate"]/fieldset/div[1]/div[2]/a')
     second_login_button.click()
+
     # Enter username, password then login
     username_field = driver.find_element(By.ID, 'username')
     username_field.send_keys('c1012902')
@@ -27,10 +29,12 @@ def automated_booking():
     password_field.send_keys('ToneExperienceEnter18102002')
     final_login_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div[3]/button')
     final_login_button.click()
+
     # Load Volleyball Booking Page
     volleyball_button = driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent__advanceSearchResultsUserControl_Activities_ctrl15_lnkActivitySelect_lg"]')
     volleyball_button.click()
-    time.sleep(1)
+    time.sleep(1)  # This sleep is necessary to allow the page to load
+
     # Move ahead 14 days
     for i in range(14):
         next_button = driver.find_element(By.ID, 'ctl00_MainContent_Button2')
@@ -38,4 +42,8 @@ def automated_booking():
 
 
 schedule.every().day.at("23:59").do(automated_booking)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
