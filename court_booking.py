9e3f254ef598da7c1c37c38edfc76922514d1662
RhysMarch <c1012902@newcastle.ac.uk>
2024-02-12 05:34:14 +0000

    update w/ fixes

--- court_booking.py
+++ court_booking.py
@@ -1,12 +1,14 @@
+import time
+import json
+import schedule
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from rich import print
-import time
-import schedule
-import json
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Username and password are stored in a json file, used for login process.
 with open('config.json') as config_file:
@@ -22,28 +24,103 @@ print(time.strftime("%d %B %Y %H:%M:%S") + " - Program Started")
 def automated_booking():
     print(time.strftime("%d %B %Y %H:%M:%S") + " - Booking Process Started")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # ChromeDriver executable
-    driver.get('https://sportsbookings.ncl.ac.uk/Connect/mrmLogin.aspx')  # Open booking website
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        driver.get('https://sportsbookings.ncl.ac.uk/Connect/mrmLogin.aspx')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    except Exception as e:
+        print(f"Error navigating to the URL: {e}")
 
     # Login buttons
-    driver.find_element(By.XPATH, '//*[@id="form_1"]/div[1]/div/a').click()
-    driver.find_element(By.XPATH, '//*[@id="Authenticate"]/fieldset/div[1]/div[2]/a').click()
+    try:
+        driver.find_element(By.XPATH, '//*[@id="form_1"]/div[1]/div/a').click()
+        driver.find_element(By.XPATH, '//*[@id="Authenticate"]/fieldset/div[1]/div[2]/a').click()
 
-    # Enter username and password, then login
-    username_field = driver.find_element(By.ID, 'username')
-    username_field.send_keys(username)
-    password_field = driver.find_element(By.ID, 'password')
-    password_field.send_keys(password)
-    final_login_button = driver.find_element(By.XPATH, '//*[@id="loginform"]/div[3]/button')
-    final_login_button.send_keys(Keys.RETURN)
+        # Enter username and password, then login
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@id="i0116"]')))
+        username_field = driver.find_element(By.XPATH, '//input[@id="i0116"]')
+        username_field.send_keys(username)
+        driver.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()  # Click the next button
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//input[@id="i0118"]')))
+        password_field = driver.find_element(By.XPATH, '//input[@id="i0118"]')
+        password_field.send_keys(password)
+        time.sleep(2)
+        driver.find_element(By.ID, "idSIButton9").click()
+    except TimeoutException:
+        print("Timed out waiting for page elements to load")
+
+    print("Please complete the authentication. Waiting for completion...")
+
+    # Element XPATH
+    auth_element_xpath = '//*[@id="idRichContext_DisplaySign"]'
+
+    # Wait for the authentication element to be visible
+    try:
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, auth_element_xpath)))
+    except TimeoutException:
+        print("Authentication prompt not found or timed out.")
+        driver.quit()
+        return
+
+    # Loop until the element is no longer visible
+    while True:
+        try:
+            # Check if the element is still visible
+            element = driver.find_element(By.XPATH, auth_element_xpath)
+            if not element.is_displayed():
+                print("Authentication complete. Continuing with the process.")
+                break
+        except:
+            # If element is not found or not visible, break the loop
+            print("Authentication complete. Continuing with the process.")
+            break
+        time.sleep(1)  # Delay to prevent too frequent checks
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "idSIButton9")))
+    driver.find_element(By.ID, "idSIButton9").click()
 
     # Load Volleyball Booking Page
-    driver.find_element(By.XPATH, '/html/body/form/div[3]/div/div/div/section/div[2]/div[1]/div[3]/div/div/div[2]/div[2]/div[47]/div[1]/div/a').click()
-    time.sleep(1)
+    try:
+        # Wait for the first element to be clickable and then click
+        recent_bookings = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="recentbookings"]/div[1]/div[1]'))
+        )
+        recent_bookings.click()
+
+        # Wait for the Volleyball Booking Page link to be clickable and then click
+        volleyball_booking_link = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//*[@id="ctl00_MainContent_MostRecentBookings1_Bookings_ctl01_bookingLink"]'))
+        )
+        volleyball_booking_link.click()
+    except TimeoutException:
+        print("Timed out waiting for the elements to be clickable.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
     # Move ahead 2 weeks (New bookings are released 14 days from now)
-    for i in range(15):
-        driver.find_element(By.ID, 'ctl00_MainContent_Button2').click()
+    try:
+        # Wait for the element to be present and click the first time
+        forward_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_MainContent_dateForward1"]'))
+        )
+        forward_button.click()
+
+        time.sleep(1)
+
+        # Wait for the element to be present again and click the second time
+        forward_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_MainContent_dateForward1"]'))
+        )
+        forward_button.click()
+    except TimeoutException:
+        print("Timed out waiting for the element to be present.")
+        # Handle the exception (e.g., retry, exit, etc.)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Handle other exceptions
 
     # Wait until 00:00:00, then refresh page to load new bookings
     while time.strftime("%H:%M:%S") != "00:00:00":
@@ -54,32 +131,37 @@ def automated_booking():
     
 ----------------------------""")
 
-    # ----------------------------------------------------------------------------------------- #
-    # //*[@id="ctl00_MainContent_grdResourceView"]/tbody/tr[7]/td/input xpath for 12:00 button  #
-    # //*[@id="ctl00_MainContent_grdResourceView"]/tbody/tr[16]/td/input xpath for 21:00 button #
-    # ----------------------------------------------------------------------------------------- #
+    time_slots_xpaths = {
+        "21:00": '//*[@id="ctl00_MainContent_cal_calbtn98"]',
+        "20:00": '//*[@id="ctl00_MainContent_cal_calbtn91"]',
+        "19:00": '//*[@id="ctl00_MainContent_cal_calbtn84"]',
+        "18:00": '//*[@id="ctl00_MainContent_cal_calbtn77"]',
+        "17:00": '//*[@id="ctl00_MainContent_cal_calbtn70"]',
+        "16:00": '//*[@id="ctl00_MainContent_cal_calbtn63"]',
+        "15:00": '//*[@id="ctl00_MainContent_cal_calbtn56"]',
+        "14:00": '//*[@id="ctl00_MainContent_cal_calbtn49"]',
+        "13:00": '//*[@id="ctl00_MainContent_cal_calbtn42"]',
+        "12:00": '//*[@id="ctl00_MainContent_cal_calbtn35"]'
+    }
 
-    xpath_base = '//*[@id="ctl00_MainContent_grdResourceView"]/tbody/tr['
-
-    # Checks 21:00 booking then works down to 12:00, will pick the first available booking
-    for slot in range(16, 6, -1):
-        xpath = xpath_base + str(slot) + ']/td/input'
+    # Loop through the time slots from 21:00 to 12:00
+    for slot, xpath in time_slots_xpaths.items():
         try:
             driver.find_element(By.XPATH, xpath).click()
-            print("| " + str(slot + 5) + ":00 slot taken         |")
+            print(f"| {slot} slot taken         |")
         except:
-            print("| " + str(slot + 5) + ":00 slot not available |")
+            print(f"| {slot} slot not available |")
 
-    # Click the confirm button to take the booking, if no booking is available, the program will go back to waiting for the next day.
+    # Click the confirm button to take the booking
     try:
         driver.find_element(By.XPATH, '//*[@id="ctl00_MainContent_btnBasket"]').click()
         print("----------------------------")
     except:
         print("""----------------------------
-        
+
     No booking available.
     Waiting for next day.
-    
+
     """)
 
     # Close the browser
